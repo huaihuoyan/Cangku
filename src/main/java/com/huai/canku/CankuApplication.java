5c5bdd637b81627c20d6ad58d569317f1c8e92df
huai <huaihuoyan@163.com>
2019-12-14 11:23:40 +0800

    创建项目

---
+++ src/main/java/com/huai/canku/CankuApplication.java
@@ -0,0 +1,13 @@
+package com.huai.canku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CankuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CankuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huai/canku/CankuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huai.canku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CankuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
